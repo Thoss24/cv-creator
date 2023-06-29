7b250de3941115dbc3ead75489d2f4a4d2863f9b
Thoss24 <t.fogarty24@outlook.com>
2023-06-29 19:46:10 +0100

    finished

--- src/components/cv_areas/CvEducation.js
+++ src/components/cv_areas/CvEducation.js
@@ -1,18 +1,35 @@
 import React, { useContext, Fragment } from "react";
 import CvContext from "../store/cv-context";
+import classes from './CvEducation.module.css';
 
 const CvEducation = () => {
-    const ctx = useContext(CvContext);
+  const ctx = useContext(CvContext);
 
-    return (
-        <Fragment>
-            <p>{ctx.universityName}</p>
-            <p>{ctx.universityCity}</p>
-            <p>{ctx.universitySubject}</p>
-            <p>{ctx.universityStartDate}</p>
-            <p>{ctx.universityEndDate}</p>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <h3>Education</h3>
+      <div className={classes['education-info-container']}>
+        <p>University name: </p>
+        <p>{ctx.universityName}</p>
+      </div>
+      <div className={classes['education-info-container']}>
+        <p>City: </p>
+        <p>{ctx.universityCity}</p>
+      </div>
+      <div className={classes['education-info-container']}>
+        <p>Subject: </p>
+        <p>{ctx.universitySubject}</p>
+      </div>
+      <div className={classes['education-info-container']}>
+        <p>From: </p>
+        <p>{ctx.universityStartDate}</p>
+      </div>
+      <div className={classes['education-info-container']}>
+        <p>To: </p>
+        <p>{ctx.universityEndDate}</p>
+      </div>
+    </Fragment>
+  );
 };
 
 export default CvEducation;
